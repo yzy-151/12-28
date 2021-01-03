e36f002fffd9043251353aa7cbaf726fe9518d8b
YZY-151 <1176514033@qq.com>
2021-01-03 21:50:45 +0800

    clone到本地试一试看报错不Jan.3

--- app/src/main/java/com/example/uiwidgettest/MainActivity3.kt
+++ app/src/main/java/com/example/uiwidgettest/MainActivity3.kt
@@ -3,12 +3,17 @@ package com.example.uiwidgettest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ProgressBar
 
 class MainActivity3 : AppCompatActivity(),View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+        val button9:Button=findViewById(R.id.button9)
+        button9.setOnClickListener {
+            finish()
+        }
     }
     override fun onClick(v:View){
         val progressBar:ProgressBar=findViewById(R.id.progressBar)
